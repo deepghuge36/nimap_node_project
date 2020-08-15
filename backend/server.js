194f0d109f202806715d842e857d22047f45f3de
DIPAK GHUGE <deepghuge@gmail.com>
2020-08-15 18:07:09 +0530

    git added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
 
-// app.get('/', (req, res) =>
-//   res.send("Get Done")
-// )
 
 const PORT = 3000;
 app.listen(PORT || process.env.PORT, () => {
